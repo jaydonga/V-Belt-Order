9a81bb049df7b2def90596495458cc4550e51d76
Jay Donga <jaydonga@gmail.com>
2025-12-26 12:18:46 +0100

    Showed default rows to each belt type

--- app/src/main/java/com/nitintraders/v_beltorder/ui/fragments/CreateNewOrderFragment.kt
+++ app/src/main/java/com/nitintraders/v_beltorder/ui/fragments/CreateNewOrderFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nitintraders.v_beltorder.R
 import com.nitintraders.v_beltorder.databinding.FragmentCreateNewOrderBinding
 import com.nitintraders.v_beltorder.ui.adapter.BeltItemAdapter
@@ -45,10 +46,28 @@ class CreateNewOrderFragment : Fragment() {
             adapterBeltTypeC.addBlankItems(blankItems)
         }
 
+        binding.layoutBeltTypeA.recyclerViewSizes.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        binding.layoutBeltTypeB.recyclerViewSizes.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+        binding.layoutBeltTypeC.recyclerViewSizes.layoutManager = LinearLayoutManager(
+            requireContext(),
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
         binding.layoutBeltTypeA.recyclerViewSizes.adapter = adapterBeltTypeA
         binding.layoutBeltTypeB.recyclerViewSizes.adapter = adapterBeltTypeB
         binding.layoutBeltTypeC.recyclerViewSizes.adapter = adapterBeltTypeC
 
-
+        binding.layoutBeltTypeA.btnAddNewItemSize.performClick()
+        binding.layoutBeltTypeB.btnAddNewItemSize.performClick()
+        binding.layoutBeltTypeC.btnAddNewItemSize.performClick()
     }
 }
