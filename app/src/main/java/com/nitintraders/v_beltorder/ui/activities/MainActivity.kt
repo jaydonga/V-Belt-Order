a2317c7cb1438ea42170c766d7bb1095e95240d3
Jay Donga <jaydonga@gmail.com>
2025-12-27 22:20:08 +0100

    Fixed back press issue, improved theme

--- app/src/main/java/com/nitintraders/v_beltorder/ui/activities/MainActivity.kt
+++ app/src/main/java/com/nitintraders/v_beltorder/ui/activities/MainActivity.kt
@@ -6,20 +6,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.nitintraders.v_beltorder.R
+import com.nitintraders.v_beltorder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         navHostFragment = supportFragmentManager.findFragmentById(R.id.main_nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
     }
 
     fun navigateToCreateNewOrder() {
--- app/src/main/java/com/nitintraders/v_beltorder/ui/fragments/HomeScreenFragment.kt
+++ app/src/main/java/com/nitintraders/v_beltorder/ui/fragments/HomeScreenFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nitintraders.v_beltorder.R
 import com.nitintraders.v_beltorder.databinding.FragmentHomeScreenBinding
 import com.nitintraders.v_beltorder.ui.activities.MainActivity
 
 class HomeScreenFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        (activity as MainActivity).supportActionBar?.title = getString(R.string.nitin_traders)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
