b2191ef80bd50e19318b6595fa7905f54b515d1b
Jay Donga <jaydonga@gmail.com>
2026-01-03 22:29:07 +0100

    Added layout for each belt order

--- app/src/main/java/com/nitintraders/v_beltorder/ui/fragments/ViewOrdersFragment.kt
+++ app/src/main/java/com/nitintraders/v_beltorder/ui/fragments/ViewOrdersFragment.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.nitintraders.v_beltorder.databinding.FragmentViewOrdersBinding
 
 class ViewOrdersFragment : Fragment() {
+
+    private lateinit var binding: FragmentViewOrdersBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+    ): View {
+        binding = FragmentViewOrdersBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
