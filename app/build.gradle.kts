85329fa640f69471bb1db3a49fef6d6b44e1bee0
Jay Donga <jaydonga@gmail.com>
2026-03-08 22:06:53 +0100

    Updated version name to 1.1.0 and version code to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.nitintraders.order"
         minSdk = 30
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
